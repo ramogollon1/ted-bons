50d398afdf60e447211b020aae73ac22e3cfe6ad
rafael <rafaelerasmo6@gmail.com>
2017-12-28 02:05:01 -0600

    header, nav, slider

--- Js/Main.js
+++ Js/Main.js
@@ -1,46 +1,76 @@
-(function() {
+	/* ------- Menu mobile --------*/
+	var theToggle = document.getElementById('toggle');
 
-var theToggle = document.getElementById('toggle');
+	function hasClass(elem, className) {
+		return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+	}
+	function addClass(elem, className) {
+	    if (!hasClass(elem, className)) {
+	    	elem.className += ' ' + className;
+	    }
+	}
+	function removeClass(elem, className) {
+		var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+		if (hasClass(elem, className)) {
+	        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+	            newClass = newClass.replace(' ' + className + ' ', ' ');
+	        }
+	        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+	    }
+	}
+	function toggleClass(elem, className) {
+		var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+	    if (hasClass(elem, className)) {
+	        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+	            newClass = newClass.replace( " " + className + " " , " " );
+	        }
+	        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+	    } else {
+	        elem.className += ' ' + className;
+	    }
+	}
+	theToggle.onclick = function() {
+	   toggleClass(this, 'on');
+	   return false;
+	}
 
-// based on Todd Motto functions
-// https://toddmotto.com/labs/reusable-js/
+	/* ------- Slider ------  */
+	var slideIndex = 1;
+	showSlides(slideIndex);
 
-// hasClass
-function hasClass(elem, className) {
-	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-}
-// addClass
-function addClass(elem, className) {
-    if (!hasClass(elem, className)) {
-    	elem.className += ' ' + className;
-    }
-}
-// removeClass
-function removeClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
-	if (hasClass(elem, className)) {
-        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
-            newClass = newClass.replace(' ' + className + ' ', ' ');
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    }
-}
-// toggleClass
-function toggleClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(" " + className + " ") >= 0 ) {
-            newClass = newClass.replace( " " + className + " " , " " );
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    } else {
-        elem.className += ' ' + className;
-    }
-}
+	// Next/previous controls
+	function plusSlides(n) {
+	  showSlides(slideIndex += n);
+	}
 
-theToggle.onclick = function() {
-   toggleClass(this, 'on');
-   return false;
-}
+	// Thumbnail image controls
+	function currentSlide(n) {
+	  showSlides(slideIndex = n);
+	}
 
-})();
+	function showSlides(n) {
+	  var i;
+	  var slides = document.getElementsByClassName("mySlides");
+	  if (n > slides.length) {slideIndex = 1} 
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+	      slides[i].style.display = "none"; 
+	  }
+	  slides[slideIndex-1].style.display = "block"; 
+	}
+
+	/* ------- Nav transparent scroll ------  */
+
+	var myNav = document.getElementById('nav-header');
+	window.onscroll = function () { 
+	    // "use strict";
+	    console.log(document.body.scrollTop);
+	    if (document.body.scrollTop >= 50 ) {
+	        myNav.classList.add("nav-colored");
+	        myNav.classList.remove("nav-transparent");
+	    } 
+	    else {
+	        myNav.classList.add("nav-transparent");
+	        myNav.classList.remove("nav-colored");
+	    }
+	};
